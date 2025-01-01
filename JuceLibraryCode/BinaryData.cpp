2392c6bb18cc01d5371953eb942b58292437b260
eclewlow <eugene.clewlow@gmail.com>
2024-12-31 23:22:07 -0800

    initial commit

---
+++ JuceLibraryCode/BinaryData.cpp
@@ -0,0 +1,108 @@
+/* ==================================== JUCER_BINARY_RESOURCE ====================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#include <cstring>
+
+namespace BinaryData
+{
+
+//================== TableData.xml ==================
+static const unsigned char temp_binary_data_0[] =
+"<TABLE_DATA>\n"
+"    <HEADERS>\n"
+"        <COLUMN columnId=\"1\" name=\"ID\" width=\"50\"/>\n"
+"        <COLUMN columnId=\"2\" name=\"Module\" width=\"200\"/>\n"
+"        <COLUMN columnId=\"3\" name=\"Name\" width=\"200\"/>\n"
+"        <COLUMN columnId=\"4\" name=\"Version\" width=\"100\"/>\n"
+"        <COLUMN columnId=\"5\" name=\"License\" width=\"100\"/>\n"
+"        <COLUMN columnId=\"6\" name=\"Groups\" width=\"50\"/>\n"
+"        <COLUMN columnId=\"7\" name=\"Dependencies\" width=\"50\"/>\n"
+"        <COLUMN columnId=\"8\" name=\"Description\" width=\"300\"/>\n"
+"        <COLUMN columnId=\"9\" name=\"Select\" width=\"50\"/>\n"
+"    </HEADERS>\n"
+"    <DATA>\n"
+"        <ITEM ID=\"01\" Module=\"juce_analytics\" Name=\"JUCE analytics classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"2\" Dependencies=\"1\" Description=\"Classes to collect analytics and send to destinations.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"02\" Module=\"juce_audio_basics\" Name=\"JUCE audio and MIDI data classes\" Version=\"5.2.0\" License=\"ISC\" Groups=\"7\" Dependencies=\"1\" Description=\"Classes for audio buffer manipulation, midi message handling, synthesis, et"
+"c.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"03\" Module=\"juce_audio_devices\" Name=\"JUCE audio and MIDI I/O device classes\" Version=\"5.2.0\" License=\"ISC\" Groups=\"3\" Dependencies=\"2\" Description=\"Classes to play and record from audio and MIDI I/O devices.\" Select"
+"=\"0\"/>\n"
+"        <ITEM ID=\"04\" Module=\"juce_audio_formats\" Name=\"JUCE audio file format codecs\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"3\" Dependencies=\"1\" Description=\"Classes for reading and writing various audio file formats.\" Sele"
+"ct=\"0\"/>\n"
+"        <ITEM ID=\"05\" Module=\"juce_audio_plugin_client\" Name=\"JUCE audio plugin wrapper classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"7\" Dependencies=\"3\" Description=\"Classes for building VST, VST3, AudioUnit, AAX and RTAS "
+"plugins.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"06\" Module=\"juce_audio_processors\" Name=\"JUCE audio processor classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"5\" Dependencies=\"2\" Description=\"Classes for loading and playing VST, AU, or internally-generated"
+" audio processors.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"07\" Module=\"juce_audio_utils\" Name=\"JUCE extra audio utility classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"3\" Dependencies=\"4\" Description=\"Classes for audio-related GUI and miscellaneous tasks.\" Select=\""
+"0\"/>\n"
+"        <ITEM ID=\"08\" Module=\"juce_blocks_basics\" Name=\"Provides low-level control over ROLI BLOCKS devices\" Version=\"5.2.0\" License=\"ISC\" Groups=\"6\" Dependencies=\"2\" Description=\"JUCE wrapper for low-level control over ROLI BLOCKS dev"
+"ices.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"09\" Module=\"juce_box2d\" Name=\"JUCE wrapper for the Box2D physics engine\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"2\" Dependencies=\"1\" Description=\"The Box2D physics engine and some utility classes.\" Select=\""
+"0\"/>\n"
+"        <ITEM ID=\"10\" Module=\"juce_core\" Name=\"JUCE core classes\" Version=\"5.2.0\" License=\"ISC\" Groups=\"16\" Dependencies=\"0\" Description=\"The essential set of basic JUCE classes, as required by all the other JUCE modules.\" Select=\"1\""
+"/>\n"
+"        <ITEM ID=\"11\" Module=\"juce_cryptography\" Name=\"JUCE cryptography classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"2\" Dependencies=\"1\" Description=\"Classes for various basic cryptography functions, including RSA, Blowfi"
+"sh, MD5, SHA, etc.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"12\" Module=\"juce_data_structures\" Name=\"JUCE data model helper classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"3\" Dependencies=\"1\" Description=\"Classes for undo/redo management, and smart data structures.\" "
+"Select=\"1\"/>\n"
+"        <ITEM ID=\"13\" Module=\"juce_dsp\" Name=\"JUCE DSP classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"5\" Dependencies=\"3\" Description=\"Classes for audio buffer manipulation, digital audio processing, filtering, oversampling,"
+" fast math functions etc.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"14\" Module=\"juce_events\" Name=\"JUCE message and event handling classes\" Version=\"5.2.0\" License=\"ISC\" Groups=\"4\" Dependencies=\"1\" Description=\"Classes for running an application's main event loop and sending/receiving"
+" messages, timers, etc.\" Select=\"1\"/>\n"
+"        <ITEM ID=\"15\" Module=\"juce_graphics\" Name=\"JUCE graphics classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"8\" Dependencies=\"1\" Description=\"Classes for 2D vector graphics, image loading/saving, font handling, etc.\" Sel"
+"ect=\"1\"/>\n"
+"        <ITEM ID=\"16\" Module=\"juce_gui_basics\" Name=\"JUCE GUI core classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"16\" Dependencies=\"3\" Description=\"Basic user-interface components and related classes.\" Select=\"1\"/>\n"
+"        <ITEM ID=\"17\" Module=\"juce_gui_extra\" Name=\"JUCE extended GUI classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"4\" Dependencies=\"1\" Description=\"Miscellaneous GUI classes for specialised tasks.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"18\" Module=\"juce_opengl\" Name=\"JUCE OpenGL classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"3\" Dependencies=\"1\" Description=\"Classes for rendering OpenGL in a JUCE window.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"19\" Module=\"juce_osc\" Name=\"JUCE OSC classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"1\" Dependencies=\"2\" Description=\"Open Sound Control implementation.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"20\" Module=\"juce_product_unlocking\" Name=\"JUCE Online marketplace support\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"2\" Dependencies=\"2\" Description=\"Classes for online product authentication.\" Select=\"0\"/>\n"
+"        <ITEM ID=\"21\" Module=\"juce_video\" Name=\"JUCE video playback and capture classes\" Version=\"5.2.0\" License=\"GPL/Commercial\" Groups=\"2\" Dependencies=\"2\" Description=\"Classes for playing video and capturing camera input.\" Select=\""
+"0\"/>\n"
+"    </DATA>\n"
+"</TABLE_DATA>\n";
+
+const char* TableData_xml = (const char*) temp_binary_data_0;
+
+
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
+{
+    unsigned int hash = 0;
+
+    if (resourceNameUTF8 != nullptr)
+        while (*resourceNameUTF8 != 0)
+            hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
+
+    switch (hash)
+    {
+        case 0x29582bd0:  numBytes = 5688; return TableData_xml;
+        default: break;
+    }
+
+    numBytes = 0;
+    return nullptr;
+}
+
+const char* namedResourceList[] =
+{
+    "TableData_xml"
+};
+
+const char* originalFilenames[] =
+{
+    "TableData.xml"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
+            return originalFilenames[i];
+
+    return nullptr;
+}
+
+}
